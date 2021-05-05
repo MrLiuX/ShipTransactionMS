157a97815d5dc2f3dcd97d2af67c63c4d1e8b3f8
MrLiuX <i@mrliux.com>
2021-05-06 02:46:34 +0800

    Complete page permission control.

--- backend/ShipTransactionMS/urls.py
+++ backend/ShipTransactionMS/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('api/',include(router.urls)),
     path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('login/', obtain_jwt_token),
-    path('login/', obtain_jwt_token),
 ]
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,12 +2,26 @@
   <div id="login_container">
     <div id="login_box">
       <p id="login_title">船舶机务管理系统</p>
-      <el-form id="login_form" :model="loginForm" :rules="login_rules" ref="loginFormRef">
+      <el-form
+        id="login_form"
+        :model="loginForm"
+        :rules="login_rules"
+        ref="loginFormRef"
+      >
         <el-form-item prop="username">
-          <el-input v-model="loginForm.username" placeholder="账号" prefix-icon="el-icon-user"></el-input>
+          <el-input
+            v-model="loginForm.username"
+            placeholder="账号"
+            prefix-icon="el-icon-user"
+          ></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" placeholder="密码" prefix-icon="el-icon-lock" type="password"></el-input>
+          <el-input
+            v-model="loginForm.password"
+            placeholder="密码"
+            prefix-icon="el-icon-lock"
+            type="password"
+          ></el-input>
         </el-form-item>
         <el-form-item id="button">
           <el-button type="primary" @click="login">登录</el-button>
@@ -20,27 +34,23 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       loginForm: {
         username: 'admin',
         password: 'admin'
       },
       login_rules: {
-        username: [
-          { required: true, message: '请输入账号', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
-        ]
+        username: [{ required: true, message: '请输入账号', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
       }
     }
   },
   methods: {
-    resetLoginForm () {
+    resetLoginForm() {
       this.$refs.loginFormRef.resetFields()
     },
-    login () {
+    login() {
       this.$refs.loginFormRef.validate(async valid => {
         if (!valid) return false
         const result = await this.$axios.post('login/', this.loginForm)
@@ -60,28 +70,28 @@ export default {
 
 <style scoped>
 #login_container {
-  background-color: #6699CC;
+  background-color: #6699cc;
   height: 100%;
 }
 
 #login_box {
-  position:absolute;
+  position: absolute;
   top: 50%;
   left: 50%;
   width: 28rem;
   height: 21rem;
   background-color: white;
   border-radius: 1rem;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 }
 
 #button {
   display: flex;
-  justify-content:flex-end;
+  justify-content: flex-end;
 }
 
 #login_form {
-  position:absolute;
+  position: absolute;
   bottom: 0;
   width: 100%;
   padding: 0 2rem;
@@ -92,6 +102,6 @@ export default {
   text-align: center;
   color: #003366;
   font-size: 1.5em;
-  font-family: "PingFang SC";
+  font-family: 'PingFang SC';
 }
 </style>
